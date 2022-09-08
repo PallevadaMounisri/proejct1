4e53f010dd0b8d99521a33b8f7f5075c33e1fae8
PallevadaMounisri <113006027+PallevadaMounisri@users.noreply.github.com>
2022-09-08 14:27:19 +0530

    added one print changes newly

--- main.py
+++ main.py
@@ -14,5 +14,6 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print('I am Mounisri')
     print('Studying 2nd year')
+    print('Welcome to github')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
